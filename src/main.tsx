0b1c19e2912339527f4fc40e94dccad42939dd71
Ahmad Raza <ahmadrazahfa@gmail.com>
2026-07-30 15:58:04 +0500

    fourth

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,27 @@
+import { createRoot } from "react-dom/client";
+import { HashRouter, Route, Routes } from "react-router";
+import App from "./app/App.tsx";
+import Home from "./app/pages/Home.tsx";
+import About from "./app/pages/About.tsx";
+import Services from "./app/pages/Services.tsx";
+import Portfolio from "./app/pages/Portfolio.tsx";
+import Booking from "./app/pages/Booking.tsx";
+import Contact from "./app/pages/Contact.tsx";
+import NotFound from "./app/pages/NotFound.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <Routes>
+      <Route element={<App />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </HashRouter>,
+);
